cfebae512999609d9f7a1788315540da09e81f97
Tanz <tanz@rose-hulman.edu>
2018-12-20 16:21:25 -0500

    Done

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -8,8 +8,8 @@ for ITERATING through SEQUENCES, including:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zichen Tan
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -77,7 +77,7 @@ def sum_radii(circles):
       :rtype: int | float
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # Note: No fair using "slices" on ANY of these problems,
@@ -88,7 +88,10 @@ def sum_radii(circles):
     #
     #       Instead, use explicit loops, as you have for other problems.
     # -------------------------------------------------------------------------
-
+    t=0
+    for i in range(len(circles)):
+        t=t+circles[i].radius
+    return t
 
 ###############################################################################
 # Some problems iterate (loop) through PART of the sequence,
@@ -152,9 +155,15 @@ def count_last_n_odds(integers, n):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+
+    count = 0
+    for i in range(len(integers)-1, len(integers)-1-n, -1):
+        if integers[i] % 2 == 1:
+            count += 1
+    return count
 
 
 ###############################################################################
@@ -227,10 +236,13 @@ def index_of_first_negative(numbers):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    for i in range(len(numbers)):
+        if numbers[i]<0:
+            return i
+    return -1
 
 def run_test_contains_an_a():
     """ Tests the   contains_an_a   function. """
@@ -285,7 +297,7 @@ def contains_an_a(s):
       :rtype: bool
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ###########################################################################
@@ -298,7 +310,10 @@ def contains_an_a(s):
     #   Use an explicit loop, as you have done in the other problems.
     #   No fair using the   count   or   find   string methods.
     # -------------------------------------------------------------------------
-
+    for i in range(len(s)):
+        if s[i] == "a":
+            return True
+    return False
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
